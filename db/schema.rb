810a2a1f6fc627c43ac3474cfe837e84b07555da
DaianaSimao <daiana.xd11@hotmail.com>
2024-04-16 13:05:32 -0300

    Feat(Schema): Schema.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_15_232130) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_16_150817) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,15 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_15_232130) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_perfis_on_user_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "titulo"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,4 +46,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_15_232130) do
   end
 
   add_foreign_key "perfis", "users"
+  add_foreign_key "posts", "users"
 end
