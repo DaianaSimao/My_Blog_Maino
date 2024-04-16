19cd57eef2e7b86a16f3f9032673aec1faf8b6cd
DaianaSimao <daiana.xd11@hotmail.com>
2024-04-15 22:39:00 -0300

    Feat(Usuário)Adiciona associação has_one :perfil e accepts_nested_attributes_for :perfil no modelo User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_one :perfil
+  accepts_nested_attributes_for :perfil
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
