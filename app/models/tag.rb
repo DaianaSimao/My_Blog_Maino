a1c65a6156890a5c053694c7d23b7d59b4010991
DaianaSimao <daiana.xd11@hotmail.com>
2024-04-20 13:41:06 -0300

    Feat(Tags): função para modifica a primeira letra da tag para maiusculo.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,9 @@
 class Tag < ApplicationRecord
   has_many :tag_posts, dependent: :destroy
+
+  before_save :capitalize_name
+
+  def capitalize_name
+    self.nome = self.nome.capitalize
+  end
 end
