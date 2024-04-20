63053ac1e286099d7336abc96e3ae71fa40cffde
DaianaSimao <daiana.xd11@hotmail.com>
2024-04-20 16:39:42 -0300

    Feat(Devise)Configação para envio de email personalizado a partir do locale i18n.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,26 @@
 class UserMailer < ActionMailer::Base
-
   def reset_password_instructions(record, token, opts = {})
+    locale = I18n.locale
+    binding.pry
     @token = token
     @resource = record
-    mail(to: record.email, subject: 'Instruções para Redefinir a Senha', **opts)
+    subject = if locale == :pt
+                'Instruções para Redefinir a Senha'
+              elsif locale == :en
+                'Instructions to Reset Your Password'
+              else
+                'Reset Password Instructions'
+              end
+
+    mail(to: record.email, subject: subject, **opts) do |format|
+      if locale == :pt
+        format.html { render "reset_password_instructions" }
+      elsif locale == :en
+        format.html { render "reset_password_instructions_en" }
+      else
+        # Renderiza uma versão padrão ou adiciona lógica adicional aqui
+        format.html { render "reset_password_instructions" }
+      end
+    end
   end
 end
