7f465ef8c25c54339d5691ea299028d7470a0061
DaianaSimao <daiana.xd11@hotmail.com>
2024-04-21 18:54:35 -0300

    Test(Post): Adiciona testes para o controller de Posts

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
-    email { 'example@example.com' }
-    password { '123456' }
+    email { Faker::Internet.email }
+    password { "123456" }
     password_confirmation { '123456' }
   end
 end
