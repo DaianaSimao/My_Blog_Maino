cbafb19eec36800a73519f8e5d7a6c0252f59809
DaianaSimao <daiana.xd11@hotmail.com>
2024-04-21 15:02:16 -0300

    Feat(Teste): Teste de models e validação do nome.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,6 @@
 class Tag < ApplicationRecord
   has_many :tag_posts, dependent: :destroy
-
+  validates :nome, presence: true, uniqueness: true
   before_save :capitalize_name
 
   def capitalize_name
