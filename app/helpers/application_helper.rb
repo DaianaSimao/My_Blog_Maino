d6231bb88862fa63f7398539f34914016ae8b2bf
DaianaSimao <daiana.xd11@hotmail.com>
2024-05-12 12:39:12 -0300

    feat: Adicionar suporte para exibição de avatar no helper ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def exibir_avatar(*args)
+    #binding.pry
+    user = User.find(args[0])
+    if user.perfil.avatar.attached?
+      if args[2].present?
+        image_tag(user.perfil.avatar, width: args[1], height: args[2], class: "rounded-circle img-fluid")
+      else
+        image_tag(user.perfil.avatar, width: args[1], class: "rounded-circle img-fluid")
+      end
+    else
+      if args[2].present?
+        image_tag("perfil.png", width: args[1], height: args[2], class: "rounded-circle img-fluid")
+      else
+        image_tag("perfil.png", width: args[1], class: "rounded-circle img-fluid")
+      end
+    end
+  end
 end
