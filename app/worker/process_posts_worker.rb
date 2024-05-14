c63b31cc9fd5ab2427b32387596ebd30142b1520
DaianaSimao <daiana.xd11@hotmail.com>
2024-05-13 21:25:00 -0300

    feat: Refatorar método perform do ProcessPostsWorker para criar vários posts de uma vez

--- app/worker/process_posts_worker.rb
+++ app/worker/process_posts_worker.rb
@@ -1,7 +1,9 @@
 class ProcessPostsWorker
   include Sidekiq::Worker
   queue_as :default
-  def perform(*args)
-    post = Post.create!(titulo: args[0], body: args[1], tag_ids: args[2], user_id: args[3])
+  def perform(posts)
+    posts.each do |post|
+      Post.create!(post)
+    end
   end
 end
