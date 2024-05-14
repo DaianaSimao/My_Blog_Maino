c185ffd82e0a7ce442425891e48fef747c5e149f
DaianaSimao <daiana.xd11@hotmail.com>
2024-05-13 22:57:53 -0300

    feat: Refatorar método perform do ProcessPostsWorker para criar um único post de cada vez

--- app/worker/process_posts_worker.rb
+++ app/worker/process_posts_worker.rb
@@ -1,9 +1,7 @@
 class ProcessPostsWorker
   include Sidekiq::Worker
   queue_as :default
-  def perform(posts)
-    posts.each do |post|
-      Post.create!(post)
-    end
+  def perform(post)
+    Post.create!(post)
   end
 end
